6c807c4410a11a4de6a706f550a7bb5b1626e39f
Yurik81 <yura.kuzub@gmail.com>
2018-06-11 16:06:25 +0300

    Net and Socket basic examples.

---
+++ src/main/java/org/hillel/chat/Examples.java
@@ -0,0 +1,79 @@
+package org.hillel.chat;
+
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+
+public class Examples {
+    public static void main(String[] args) throws UnknownHostException {
+
+//        getUrlConnection();
+
+
+//        getMyIPadress();
+//        createIP();
+
+    }
+
+    private static void getUrlConnection() {
+        String urlName = "https://www.oracle.com";
+        int timeout = 10_000_000;
+        URL url;
+        try {
+            url = new URL(urlName);
+            final URLConnection connection = url.openConnection();
+            // установка таймаута на соединение
+            connection.setConnectTimeout(timeout);
+            InputStream is = connection.getInputStream();
+
+            BufferedInputStream bin = new BufferedInputStream(is);
+            byte[] buffer = new byte[bin.available()];
+            // считаем файл в буфер
+            bin.read(buffer, 0, bin.available());
+
+            System.out.println(" data:");
+            for (int i = 0; i < buffer.length; i++) {
+                System.out.print((char) buffer[i]);
+            }
+
+
+            // продолжение извлечения информации из соединения
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createIP() {
+        byte ip[] = {(byte) 81, (byte) 25, (byte) 231, (byte) 118};
+        try {
+            InetAddress addr = InetAddress.getByAddress("[omr.gov.ua]", ip);
+            System.out.println(addr.getHostName() + "-> cоединение:" + addr.isReachable(2000));
+        } catch (UnknownHostException e) {
+            System.err.println("адрес недоступен " + e);
+        } catch (IOException e) {
+            System.err.println("ошибка потока " + e);
+        }
+    }
+
+    private static void getMyIPadress() {
+        InetAddress currentIP = null;
+        InetAddress bsuIP = null;
+        try {
+            currentIP = InetAddress.getLocalHost();
+            // вывод IP-адреса локального компьютера
+            System.out.println("Мой IP -> " + currentIP.getHostAddress());
+            bsuIP = InetAddress.getByName("www.google.com");
+            // вывод IP-адреса www.google.com
+            System.out.println("google -> " + bsuIP.getHostAddress());
+        } catch (UnknownHostException e) {
+            // если не удается найти IP
+            System.err.println("адрес недоступен " + e);
+        }
+    }
+
+
+}
---
+++ src/main/java/org/hillel/chat/SimpleClientSocket.java
@@ -0,0 +1,33 @@
+package org.hillel.chat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SimpleClientSocket {
+    public static void main(String[] args) {
+
+        try {
+            Socket socket = new Socket("localhost", 8030);
+
+            try (InputStream in = socket.getInputStream();
+                 OutputStream out = socket.getOutputStream()) {
+
+                String messageRequest = "Hello my dear Server";
+                out.write(messageRequest.getBytes());
+                out.flush();
+
+                byte[] messageResponse = new byte[in.available()];
+                int readBytes = in.read(messageResponse);
+
+                System.out.println("Server response me: " +
+                        new String(messageResponse, 0, readBytes));
+
+            }
+
+        } catch (IOException e) {
+            System.err.println("ошибка: " + e);
+        }
+    }
+}

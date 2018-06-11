2ca2b2406fc013860e950c035dcb0b9c1ccba103
Yurik81 <yura.kuzub@gmail.com>
2018-06-11 18:57:28 +0300

    Draft chat programm

---
+++ src/main/java/org/hillel/chat/org/hilllel/chat/thread/NetClient.java
@@ -0,0 +1,49 @@
+package org.hillel.chat.org.hilllel.chat.thread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class NetClient {
+
+    public static void main(String[] args) {
+        Socket socket = null;
+        BufferedReader br = null;
+        try {
+            // установка соединения с сервером
+            socket = new Socket(InetAddress.getLocalHost(), 8071);
+            // или Socket socket = new Socket("ИМЯ_СЕРВЕРА", 8071);
+            PrintStream ps = new PrintStream(socket.getOutputStream());
+
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            ps.println("/HELP");
+            System.out.println(br.readLine());
+
+        } catch (UnknownHostException e) {
+            // если не удалось соединиться с сервером
+            System.err.println("адрес недоступен" + e);
+        } catch (IOException e) {
+            System.err.println("ошибка I/О потока" + e);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/org/hillel/chat/org/hilllel/chat/thread/ServerThread.java
@@ -0,0 +1,54 @@
+package org.hillel.chat.org.hilllel.chat.thread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+class ServerThread extends Thread {
+    private PrintStream os; // передача
+    private BufferedReader is; // прием
+    private InetAddress addr; // адрес клиента
+
+    public ServerThread(Socket s) throws IOException {
+        os = new PrintStream(s.getOutputStream());
+        is = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        addr = s.getInetAddress();
+    }
+
+    public void run() {
+        int i = 0;
+        String str;
+        try {
+            while ((str = is.readLine()) != null) {
+                if ("/HELP".equals(str)) {
+                    os.println("Hello, Welcome to my super chat");
+                }
+                System.out.println("Message responded by server name: " + addr.getHostName());
+            }
+        } catch (IOException e) {
+            // если клиент не отвечает, соединение с ним разрывается
+            System.err.println("Disconnect");
+        } finally {
+            disconnect(); // уничтожение потока
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (os != null) {
+                os.close();
+            }
+            if (is != null) {
+                is.close();
+            }
+            System.out.println(addr.getHostName() + " disconnecting");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            this.interrupt();
+        }
+    }
+}
